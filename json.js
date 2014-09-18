34ab87a80673684ca5355763fe05d18f5100e30b
mvoshell78 <michael.voshell@yahoo.com>
2014-09-18 18:06:45 -0400

    added json data
    added a new js file and added json data

---
+++ json.js
@@ -0,0 +1,32 @@
+//alert("JavaScript works!");
+//Michael Voshell
+//SDI 092014
+// Project 3
+// json data
+
+var json = {
+       "dogs" : [
+         {
+                  "name"   : "Sam",
+                  "age"    : 8,
+                  "breed"  : " Boxer"      
+         },
+         {
+                  "name"   : "Pedro",
+                  "age"    : 8,
+                  "breed"  : " Shitszu" 
+         },
+         {
+                  "name"   : "Fiona",
+                  "age"    : 6,
+                  "breed"  : " Boxer" 
+         },
+         {
+                  "name"   : "Tyson",
+                  "age"    : 3,
+                  "breed"  : " Boxer" 
+         }
+       ]
+       
+};
+
