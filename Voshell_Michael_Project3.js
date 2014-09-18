f8f949fdec34fa4cdc34fe9119f489989067af97
mvoshell78 <michael.voshell@yahoo.com>
2014-09-18 17:43:53 -0400

    loops, string, math, boolean
    struggled with nested loops
    added string, math, boolean function

--- Voshell_Michael_Project3.js
+++ Voshell_Michael_Project3.js
@@ -10,19 +10,30 @@ var dogNames = ["Sam",
                 "Fiona",
                 "Tyson"];
 
+var enjoys = ["Sleeping",
+               "Eating",
+               "Playing"];
 
+           
 // Functions and Procedures
+
+
 
 var say = function(message) {console.log(message); };// simplified console.log
 
-var myDogs = function(dogNames) {
-       
-        for ( var dogs = 0; dogs < dogNames.length; dogs++) {
-       say("I have " + dogNames.length + " dogs, one named " + dogNames[dogs] )
-      
-        }; // for loop
+var myDogs = function(dogNames, enjoys) {
+         for ( var dogs = 0; dogs < dogNames.length; dogs++) {
+            for (var i = 0; i < enjoys.length; i++) {
+            say("I have " + dogNames.length + " dogs, one named " + dogNames[dogs] )
+                if (dogNames[dogs] === "Fiona") {
+                    say (dogNames[dogs] + " enjoys " + enjoys[i] + ", because she's a dog and doesn't have anything better to do"   )
+                } else {
+                    say (dogNames[dogs] + " enjoys " + enjoys[i] + ", because he's a dog and doesn't have anything better to do"  )
+                }; // end of conditional
+            }; // Nested loop
+        }; // for loop 
+        }; // End of procedure
     
-};// end of procedure
 
 // string function with return using array
 
@@ -33,14 +44,54 @@ var didNotHave = function(dogNames) {
  
 };
 
+// Math function with return value
+
+var combinedAges = function(dogAges) {
+    var agesMath = dogAges[0] + dogAges[1] + dogAges[2] + dogAges [3]
+    return(agesMath)
+    
+};
+
+var dogYears = function(ages) {
+    var dogyears = ages * 7
+    return dogyears
+    
+};
+
+// boolean arguement with return value
+var howMany = function(dogAges) {
+    if (dogAges.length === 4  ) {
+        return true
+    } else { return false 
+        };
+    };
+
+
+
+
+
+
 // Main Code
 
 
-myDogs(dogNames)
+myDogs(dogNames, enjoys)
+
+
+
+var isitTrue = howMany(dogAges)
+    if (isitTrue === true ) {
+        say (" Four is alot ")
+    } else {
+        say (" Why did you lie to me ?")
+        };
+
 
 var string = didNotHave(dogNames)
     say (string)
 
 
+var ages = combinedAges(dogAges)
+    say ("Their combined ages are " + ages + " years old" )
 
-
+var dogYear = dogYears(ages)
+    say ("Thats " + dogYear + " In dog years! ")
