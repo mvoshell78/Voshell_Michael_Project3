1de4af00e1fbf84ca7a2fcf5094c748443cda7e0
mvoshell78 <michael.voshell@yahoo.com>
2014-09-17 22:38:30 -0400

    added string function with return
    added the string function with return variable

--- Voshell_Michael_Project3.js
+++ Voshell_Michael_Project3.js
@@ -20,16 +20,26 @@ var myDogs = function(dogNames) {
         for ( var dogs = 0; dogs < dogNames.length; dogs++) {
        say("I have " + dogNames.length + " dogs, one named " + dogNames[dogs] )
       
-        }; // for loop loop
+        }; // for loop
     
 };// end of procedure
 
+// string function with return using array
+
+var didNotHave = function(dogNames) {
+    var string = "We didn't always have "
+    notSoMany = ( string + dogNames )
+    return notSoMany
+ 
+};
 
 // Main Code
 
 
 myDogs(dogNames)
 
+var string = didNotHave(dogNames)
+    say (string)
 
 
 
