be5796dc73ab0abe619b507d6d834a4ea8dbdf48
mvoshell78 <michael.voshell@yahoo.com>
2014-09-17 22:21:25 -0400

    finished while loop

--- Voshell_Michael_Project3.js
+++ Voshell_Michael_Project3.js
@@ -5,53 +5,32 @@
 
 
 //Global Variables
-var dogNames = ["Sam","Pedro","Fiona","Tyson"];
+var dogNames = ["Sam",
+                "Pedro",
+                "Fiona",
+                "Tyson"];
+
 
 // Functions and Procedures
 
-var say = function(message) {console.log(message); };// simplified console log
+var say = function(message) {console.log(message); };// simplified console.log
 
-var myDogs = function(whatName) {
-    dogsNames = dogNames[whatName];
-     for (var name = 0; name > dogsNames.length; name ++ ) {
-       say("I have four dogs one named " + dogsNames ) 
-     };
+var myDogs = function(dogNames) {
+       
+        for ( var dogs = 0; dogs < dogNames.length; dogs++) {
+       say("I have " + dogNames.length + " dogs, one named " + dogNames[dogs] )
+      
+        }; // for loop loop
     
-};
+};// end of procedure
 
 
 // Main Code
 
 
+myDogs(dogNames)
 
 
 
 
 
-
-/*
-var tankNames = ["Shark Tank",
-                 "Sting Ray Tank",
-                 "Dolphin Tank",
-                 "Fish Tank"],
-    minutesPerTank = [20, 15, 25, 30],
-    kidNames = ["Ronnie", "Bobbi", "Micky", "Mike"];
-    
-var visitTank = function(whatTank) {
-    var tankName = tankNames[whatTank],
-        minutesThisTank = minutesPerTank[tankNumber];
-    console.log(kidNames[0] + " wanted us to visit the " + tankName +
-        " for " + minutesThisTank + " minutes. So I set the timer on" +
-        " my watch!");
-    for (var minutes = 0; minutes < minutesThisTank; minutes += 5) {
-            var minutesRemain = minutesThisTank - minutes;
-            console.log("We have " + minutesRemain + " minutes left. " +
-                minutes + " minutes have past.");
-        };
-    console.log("We finished with the " + tankName + ".");
-};
-
-for (var tankNumber = 0; tankNumber < tankNames.length; tankNumber++) {
-    visitTank(tankNumber);
-};
-*/
